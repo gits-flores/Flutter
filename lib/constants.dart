bf6f9cfcef3de346902ee7b5832ebb1ebf08e719
Muhammad Yusuf <muhammad.yusuf18010@student.unsika.ac.id>
2021-11-12 04:05:12 +0700

    menambahkan onboarding screen, dengan baik dan benar

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,9 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
+Color kblue = Color(0xFF4756DF);
+Color kwhite = Color(0xFFFFFFFF);
+Color kblack = Color(0xFF000000);
+Color kbrown300 = Color(0xFF8D6E63);
+Color kbrown = Color(0xFF795548);
+Color kgrey = Color(0xFFC0C0C0);
